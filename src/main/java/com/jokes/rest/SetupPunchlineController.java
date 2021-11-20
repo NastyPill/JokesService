6b4e50fc7e26d57563c53b47207461fd899ad0c6
NastyPill <DJPonnYash.Vinyl@gmail.com>
2021-11-20 14:14:25 +0300

    add support of getting random S-P joke

--- src/main/java/com/jokes/rest/SetupPunchlineController.java
+++ src/main/java/com/jokes/rest/SetupPunchlineController.java
@@ -1,17 +1,27 @@
 package com.jokes.rest;
 
+import com.jokes.service.SetupPunchlineService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.net.http.HttpResponse;
+import java.io.IOException;
 
 @RestController
 public class SetupPunchlineController {
 
+    @Autowired
+    SetupPunchlineService setupPunchlineService;
+
 
     @GetMapping(path = "/sp/get")
-    public HttpResponse<String> getRandom() {
-        throw new UnsupportedOperationException();
+    public ResponseEntity<String> getRandom() {
+        try {
+            return ResponseEntity.ok(setupPunchlineService.getRandom());
+        } catch (IOException e) {
+            return ResponseEntity.internalServerError().body("Something went wrong");
+        }
     }
 
 
