bda1eb6531bf6340155f964b91e940a69e846e40
NastyPill <DJPonnYash.Vinyl@gmail.com>
2022-01-16 21:00:08 +0300

    add tests for CI

--- src/main/java/com/jokes/service/FatherStylesService.java
+++ src/main/java/com/jokes/service/FatherStylesService.java
@@ -2,14 +2,12 @@ package com.jokes.service;
 
 import org.springframework.stereotype.Service;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 @Service
-public class FatherStylesService {
+public class FatherStylesService extends JokesService {
 
     public String getRandom() throws IOException {
         URL url = new URL("https://icanhazdadjoke.com");
@@ -18,13 +16,7 @@ public class FatherStylesService {
         connection.setRequestProperty("Accept", "text/plain");
         int status = connection.getResponseCode();
         System.out.println(status);
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        StringBuilder result = new StringBuilder();
-        String inputLine;
-        while ((inputLine = bufferedReader.readLine()) != null) {
-            result.append(inputLine);
-        }
-        return result.toString();
+        return getStringFromInputStream(connection.getInputStream());
     }
 
 }
--- src/main/java/com/jokes/service/SetupPunchlineService.java
+++ src/main/java/com/jokes/service/SetupPunchlineService.java
@@ -2,14 +2,12 @@ package com.jokes.service;
 
 import org.springframework.stereotype.Service;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 @Service
-public class SetupPunchlineService {
+public class SetupPunchlineService extends JokesService {
 
     public String getRandom() throws IOException {
         URL url = new URL("https://official-joke-api.appspot.com/random_joke");
@@ -17,13 +15,7 @@ public class SetupPunchlineService {
         connection.setRequestMethod("GET");
         int status = connection.getResponseCode();
         System.out.println(status);
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-        StringBuilder result = new StringBuilder();
-        String inputLine;
-        while ((inputLine = bufferedReader.readLine()) != null) {
-            result.append(inputLine);
-        }
-        return result.toString();
+        return getStringFromInputStream(connection.getInputStream());
     }
 
 
